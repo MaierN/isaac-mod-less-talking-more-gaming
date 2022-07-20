a15835ea2030778c1905ccfbd02b831cc1991f0a
Nicolas Maier <20108526+MaierN@users.noreply.github.com>
2022-07-20 11:14:53 +0200

    .

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { saveDataManager } from "isaacscript-common";
 
 export const MOD_NAME = "Less talking. More gaming.";
-export const MOD_VERSION = "1.3";
+export const MOD_VERSION = "1.7";
 
 export const config = {
   persistent: {
